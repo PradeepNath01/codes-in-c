f9f68b973c8cfcb216529d638c9ad939b3ea8b22
Pradeep Nath <174758724+PradeepNath01@users.noreply.github.com>
2025-03-11 19:53:55 +0545

    Add files via upload

---
+++ arthemetic_operator.c
@@ -0,0 +1,11 @@
+/*
+write a program that asked for an arthmetic operator and two 
+operands and perform the correspomdimg operation  the operands*/
+#include <stdio.h>
+#include <math.h>
+#include<conio.h>
+{
+    char ch:
+    int a , b , c;
+    
+}
